1b6f48994e5a2a71f27c7dcd53a97a7cb2f8b714
azimjonkamolov <azimjonkamolovuz@gmail.com>
2021-02-13 21:35:33 +0900

    clean

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -1,3 +1,43 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("gotoTop").style.display = "block";
+	} else {
+		document.getElementById("gotoTop").style.display = "none";
+	}
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	$('html, body').animate({scrollTop:0}, 'slow');
+}
+var timeOnEachText1 = 2000; // Milliseconds to spend on each before moving to next
+var text1 = [' Azimjon ', ' Problem ', 'Web'];
+var counter1 = 0;
+var elem1 = document.getElementById("fadeOne");
+function changeOne() {
+  jQuery(elem1).delay(timeOnEachText1).fadeTo(2000, 0, function() {
+	this.innerHTML = text1[counter1];
+	counter1 = ++counter1 % text1.length;
+	jQuery(this).fadeTo(2000, 1, changeOne)
+  })
+}
+changeOne();
+
+var timeOnEachText2 = 2000; // Milliseconds to spend on each before moving to next
+var text2 = ['Kamolov', ' Solver ', 'Developer'];
+var counter2 = 0;
+var elem2 = document.getElementById("fadeTwo");
+function changeTwo() {
+  jQuery(elem2).delay(timeOnEachText2).fadeTo(2000, 0, function() {
+	this.innerHTML = text2[counter2];
+	counter2 = ++counter2 % text2.length;
+	jQuery(this).fadeTo(2000, 1, changeTwo)
+  })
+}
+changeTwo();
+
+
 
 (function($) {
 
